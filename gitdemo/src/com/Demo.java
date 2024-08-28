7b17b586eb8717c7cdfdb4f79c8bf9135052644a
pratikMane07 <pratiksmane07@gmail.com>
2024-08-28 15:25:53 +0530

    add m1 by amit sir

--- gitdemo/src/com/Demo.java
+++ gitdemo/src/com/Demo.java
@@ -2,9 +2,14 @@ package com;
 
 public class Demo {
 
+	public void m1() {
+		System.out.println("This is m1 method..!");
+
+	}
+
 	public static void main(String[] args) {
-		
-		System.out.println("This is Git Demo...!");
+
+		System.out.println("This is Git Demo... !");
 
 	}
 
